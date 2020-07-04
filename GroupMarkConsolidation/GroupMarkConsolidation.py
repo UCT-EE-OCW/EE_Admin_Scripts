02000305f2fcebb0b809aa9f1a9ebcc5e8244be0
Keegan Crankshaw <kcranky@gmail.com>
2020-07-04 10:18:24 +0200

    Added GroupMarkConsolidation script

---
+++ GroupMarkConsolidation/GroupMarkConsolidation.py
@@ -0,0 +1,54 @@
+import csv
+import os
+
+# WE need a field called "studnums" in each?
+
+def process(csv_name):
+    # load in the CSV
+    csv_in = open(csv_name)
+    reader_main = csv.DictReader(csv_in)
+
+    # create the output csv
+    fieldnames = reader_main.fieldnames
+    fieldnames = ["studnum", "partners"] + fieldnames
+    fieldnames.remove("studnums")
+
+    csv_out = open("{}_processed.csv".format(csv_name[:-4]), 'w', newline='')
+    writer = csv.DictWriter(csv_out, fieldnames=fieldnames)
+    writer.writeheader()
+
+    temp_arr = []
+
+    # Split and add
+    for i, row in enumerate(reader_main):
+        studnums = row["studnums"].replace(" ", "").split(",")
+        for s in studnums:
+            if s is not None and s != "":
+                cpy = studnums.copy()
+                cpy.remove(s)
+                tmp = {
+                    "studnum": s,
+                }
+                tmp.update(row)
+                tmp.pop("studnums")
+                tmp["partners"] = cpy
+                temp_arr.append(tmp)
+
+    # sort list by studnum
+    temp_arr = sorted(temp_arr, key=lambda i: i["studnum"])
+
+    # write to file
+    writer.writerows(temp_arr)
+
+    # Finally, close the files
+    csv_out.close()
+    csv_in.close()
+
+
+if __name__ == "__main__":
+    root_dir = "."
+    entries = os.listdir(root_dir)
+    for entry in entries:
+        if entry[-4:] == ".csv":
+            csv_name = root_dir + '/' + entry
+            process(csv_name)
---
+++ GroupMarkConsolidation/README.md
@@ -0,0 +1,6 @@
+# Group Mark Consolidation
+Raeds in a csv and duplicates entries for each student number. Speeds up admin/marking as no manual copy/paste is required.
+
+Example:
+- Place a csv called "Assignment 3.csv" in the same folder as the script with a column called "studnums"
+- The script will create a new csv ("Assignment 3_processed.csv") with a row per entry of each student number in the "studnums" column
